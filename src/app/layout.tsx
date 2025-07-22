6f13205f9325e0458d61bdba37ee4df02110d022
halifox <106858061+halifox@users.noreply.github.com>
2025-07-22 14:51:38 +0800

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "./globals.css";
+import {Metadata} from "next";
 
-
+export const metadata: Metadata = {
+    title: 'ProGuard 反混淆工具',
+    description: '在线解析和还原 ProGuard 混淆的类与方法，支持批量映射处理',
+    icons: {
+        icon: '/favicon.svg',
+    },
+}
 export default function RootLayout({children,}: Readonly<{
     children: React.ReactNode;
 }>) {
