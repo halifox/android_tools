5483fd61ef8310b1b9d0254454e9c3facae99067
halifox <106858061+halifox@users.noreply.github.com>
2025-07-22 10:36:47 +0800

    Implement core functions and UI

---
+++ src/app/actions.tsx
@@ -0,0 +1,47 @@
+'use server'
+import fs from 'fs'
+import path from 'path'
+import {v4 as uuidv4} from 'uuid'
+import {pipeline} from 'stream/promises'
+import {Readable} from 'stream'
+import {exec} from 'child_process';
+
+export async function uploadMapping(formData: FormData) {
+    const file = formData.get('file') as File
+
+    const mapping = uuidv4();
+    const mappingPath = path.resolve('./uploads', `${mapping}.txt`)
+    await fs.promises.mkdir(path.dirname(mappingPath), {recursive: true})
+
+    const source = Readable.fromWeb(file.stream());
+    const destination = fs.createWriteStream(mappingPath)
+    await pipeline(source, destination)
+
+    return mapping
+}
+
+export async function retrace(mapping: string, obfuscated: string) {
+    const mappingPath = path.resolve('./uploads', `${mapping}.txt`);
+    const obfuscatedPath = path.resolve('./uploads', `${uuidv4()}.txt`);
+    const deobfuscatedPath = path.resolve('./uploads', `${uuidv4()}.txt`);
+
+    await new Promise((resolve, reject) => {
+        fs.writeFile(obfuscatedPath, obfuscated, {encoding: 'utf-8'}, (err) => {
+            if (err) return reject(err);
+            resolve(null);
+        });
+    })
+
+    await new Promise((resolve, reject) => {
+        exec(`retrace ${mappingPath} ${obfuscatedPath} > ${deobfuscatedPath}`, (err) => {
+            if (err) return reject(err);
+            resolve(null);
+        })
+    })
+    return await new Promise<string>((resolve, reject) => {
+        fs.readFile(deobfuscatedPath, 'utf-8', (err, data) => {
+            if (err) return reject(err);
+            resolve(data);
+        })
+    })
+}
