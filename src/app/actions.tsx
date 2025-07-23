0ef4222c0a0e1c3b47df98edf4ad05973093df7a
halifox <106858061+halifox@users.noreply.github.com>
2025-07-23 10:19:17 +0800

    clear code

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -2,28 +2,9 @@
 import fs from 'fs'
 import path from 'path'
 import {v4 as uuidv4} from 'uuid'
-import {pipeline} from 'stream/promises'
-import {Readable} from 'stream'
-import {ReadableStream} from 'stream/web'
 import {exec} from 'child_process';
 
-export async function uploadMapping(formData: FormData) {
-    console.log("uploadMapping", formData)
-    const file = formData.get('file') as File
-
-    const mapping = uuidv4();
-    const mappingPath = path.resolve('./uploads', `${mapping}.txt`)
-    await fs.promises.mkdir(path.dirname(mappingPath), {recursive: true})
-
-    const source = Readable.fromWeb(file.stream() as ReadableStream);
-    const destination = fs.createWriteStream(mappingPath)
-    await pipeline(source, destination)
-
-    return mapping
-}
-
 export async function retrace(mapping: string, obfuscated: string) {
-    console.log("retrace", mapping, obfuscated)
     const mappingPath = path.resolve('./uploads', `${mapping}.txt`);
     const obfuscatedPath = path.resolve('./uploads', `${uuidv4()}.txt`);
     const deobfuscatedPath = path.resolve('./uploads', `${uuidv4()}.txt`);
